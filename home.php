28c730391bf82ed64cdbac0a73c900fb7b6e6286
AnastasiiaNA <anedilko3@gmail.com>
2018-12-07 20:33:58 +0200

    *try to add base routing

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 007 07.12.18
+ * Time: 20:31
+ */
+?>
+<p>Home</p>
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 007 07.12.18
+ * Time: 19:42
+ */
+?>
+
+<p> test </p>
